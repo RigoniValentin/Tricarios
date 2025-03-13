df158e25755396f70e5d339ba1d125ceecd2363f
unknown <rigonitomas@outlook.com>
2025-03-13 03:11:43 -0300

    Actualización del código y ajustes

--- src/config/mongodb.ts
+++ src/config/mongodb.ts
@@ -3,14 +3,18 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const mongoDbUrl = process.env.MONGODB_URL_STRING as string;
+const mongoDbUrl = process.env.MONGODB_URL_STRING;
+if (!mongoDbUrl) {
+  console.error("MONGODB_URL_STRING is not defined in your environment.");
+  process.exit(1);
+}
 
 export default (async () => {
   try {
     await mongoose.connect(mongoDbUrl);
     console.log("Conectado a MongoDB");
   } catch (error) {
-    console.log("Erroral conectar", error);
+    console.error("Erroral conectar", error);
     process.exit(1);
   }
 })();
